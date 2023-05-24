c660db1f6c4b559e622e4f23ae8ec46624a6801c
your name <a9tttungdz@gmail.com>
2023-05-24 15:00:48 +0700

    #8 create: home-page v1

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,18 @@
 import type { NextPage } from "next";
 
+import Header from "./Layout/header";
+
+
+
 const Home: NextPage = () => {
-  return <div className="text-red-500">Bootstrap DGU</div>;
+  return (
+    <div className="">
+      <Header />
+      <div className="text-red-500">Bootstrap DGU</div>
+
+    
+    </div>
+  )
 };
 
 export default Home;
