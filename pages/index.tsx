d89d8d68a88e2a1dac3ddd42e8c1ff54e55bc7ac
Jocelyn Lam <94306642+lamxuanthinh@users.noreply.github.com>
2023-05-26 21:45:57 +0700

    Revert "Setup tailwind"

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <div className="text-red-500">Bootstrap DGU</div>;
+  return <div>Bootstrap DGU</div>
 };
 
 export default Home;
