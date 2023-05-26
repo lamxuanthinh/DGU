06a34e570a06e796b0da1c7492d5dd0699d7fd47
lamxuanthinh <thinhlam749@gmail.com>
2023-05-26 23:40:56 +0700

    #7 Fix Bug

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <div className="text-red-500">Bootstrap DGU</div>;
+  return <div className="text-red-500">Bootstrap DGU GIT</div>;
 };
 
 export default Home;
