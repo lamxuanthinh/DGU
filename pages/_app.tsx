56d8ea014c4de5729cbbbd4ec6bdbaec8161ddc0
Giang-st1-00 <bin17022003@gmail.com>
2023-06-08 14:52:58 +0700

    update: commit test (#8)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,7 @@ import { useEffect, useMemo, useState } from "react";
 
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const Layout = Component.Layout ?? EmptyLayout;
+  console.log(3);
 
   return (
     <Layout>
