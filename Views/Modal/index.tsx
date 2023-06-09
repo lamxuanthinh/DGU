f783bef089fcd37247a7c29cc31aeb07b7e85a59
DuyTinh2003 <tranduytinh258@gmail.com>
2023-06-09 14:58:04 +0700

    feat: add component button (#63)

--- Views/Modal/index.tsx
+++ Views/Modal/index.tsx
@@ -1,3 +1,4 @@
+import Button from "@/components/common/Button";
 interface IModalProps {
   title: string;
   onOk: () => void;
@@ -7,20 +8,20 @@ export default function Modal({ title, onOk, onCancel }: IModalProps) {
   return (
     <div className="fixed inset-0 flex items-center justify-center transition-opacity z-50 bg-modal-gradient backdrop-blur-sm font-semibold select-none">
       <div className="">
-        <p className="text-3xl text-[#DBDBDB]  mb-14">{title}</p>
+        <p className="text-3xl text-[#DBDBDB]  mb-14 text-center">{title}</p>
         <div className="flex justify-center text-[26px] text-black ">
-          <button
-            className="rounded-[16px] mr-20 bg-white min-w-[200px] py-5 hover:opacity-80 transition-opacity duration-200"
+          <Button
+            className="rounded-[16px] mr-20 !min-w-[200px] py-5 text-[26px]"
             onClick={onOk}
           >
             OK
-          </button>
-          <button
-            className="rounded-[16px]  bg-white min-w-[200px] py-5 hover:opacity-80 transition-opacity duration-200"
+          </Button>
+          <Button
+            className="rounded-[16px] mr-20 !min-w-[200px] py-5 text-[26px]"
             onClick={onCancel}
           >
             Cancel
-          </button>
+          </Button>
         </div>
       </div>
     </div>
