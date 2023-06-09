793a9d337c31a73bccbb97dc70436b6b5a4d738f
your name <a9tttungdz@gmail.com>
2023-06-09 08:35:06 +0700

    Completed Feature Upload Video File (#39)

--- Views/Upload/index.tsx
+++ Views/Upload/index.tsx
@@ -1,12 +1,12 @@
-import React, { HtmlHTMLAttributes, useRef, useState } from "react";
-import fileType from "file-type";
-// import ClamAV from "clamav.js";
+import axios from "axios";
+import React, { useRef, useState } from "react";
+
 
 
 import { FaCloudUploadAlt } from "react-icons/fa";
 
 
-// handel if file is not a video file.
+// handle if file is not a video file.
 function handleFileSelect(videoFile: File) : boolean {
     const file = videoFile;
 
@@ -27,20 +27,85 @@ function handleFileSelect(videoFile: File) : boolean {
     }
 }
 
+// handle check viruss video file.  
+async function checkFileForViruses(file: File): Promise<boolean> {
+
+    // upload file on VirussTotal
+    const url = `https://www.virustotal.com/api/v3/files`;
+    const formData = new FormData();
+    formData.append('file', file)
+    const response = await axios.post(url, formData, {
+        headers: {
+            'accept': 'application/json',
+            'x-apikey': '50d5e74f6630f739b2c71da3d34baab3b4e9a1dbf748a9721d60014f3ef2b4ef',
+            'Content-Type': 'multipart/form-data',
+        }
+    });
+    const data = response.data;
+    console.log(data);
+    console.log(data.data.links.self);
+
+    
+    
+    // get md5 analysis file on VirusTotal
+    const urlAnalysis : string = data.data.links.self;
+    const responeAnalysis = await axios.get(urlAnalysis, {
+        headers: {
+            'x-apikey': '50d5e74f6630f739b2c71da3d34baab3b4e9a1dbf748a9721d60014f3ef2b4ef',
+        }
+    });
+    console.log(responeAnalysis);
+    console.log(responeAnalysis.data.meta.file_info.md5);
+
+    
+    const md5File = responeAnalysis.data.meta.file_info.md5;
+
+
+
+    // get result file on VirusTotal
+    const urlDetect = url + '/' + md5File;
+    const responeDetect = await axios.get(urlDetect, { 
+        headers: {
+            'x-apikey': '50d5e74f6630f739b2c71da3d34baab3b4e9a1dbf748a9721d60014f3ef2b4ef',
+        }
+    });
+    console.log(responeDetect.data.data.attributes.last_analysis_stats.malicious);
+
+    const hasViruss : number = responeDetect.data.data.attributes.last_analysis_stats.malicious;
+    
+    if(hasViruss > 0)
+    {
+        return true; // This mean has the viruss in this file.
+    }   
+    else
+    {
+        return false; // This mean hasn't the viruss in this file.
+    }
+}
+
+
+
 
 export default function index() {
 
-    // import
+    // IMPORT
         // create reference variable.
     // eslint-disable-next-line react-hooks/rules-of-hooks
     const  inputRef = useRef<HTMLInputElement>(null);
+
         // create files to drag and drop.
     // eslint-disable-next-line react-hooks/rules-of-hooks
     const [isDragging, setIsDragging] = useState(false);
     // eslint-disable-next-line react-hooks/rules-of-hooks
     const [selectedVideo, setSelectedVideo] = useState<File | null>(null);
 
+        // import auseUsestate  variable to loading when checking viruss through API.
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const [loading, setLoading] = useState(false);
 
+
+
+    // HANDLE
     // handle drag and drop file
     const handleDragOver = (event: React.DragEvent<HTMLDivElement>) => {
         event.preventDefault();
@@ -51,7 +116,7 @@ export default function index() {
         setIsDragging(false);
     };
 
-    const handleDrop = (event: React.DragEvent<HTMLDivElement>) => {
+    const handleDrop = async (event: React.DragEvent<HTMLDivElement>) => {
         event.preventDefault();
         setIsDragging(false);
     
@@ -68,6 +133,30 @@ export default function index() {
                 if(handleFileSelect(inputRef.current.files[0]) === true)
                 {
                     alert('This is video file.');
+                    
+                    const waitingToLoading = async (inputRef : HTMLInputElement) => {
+                        try {
+                            setLoading(true);
+                            const hasVirus = await checkFileForViruses(files[0]);
+                            setLoading(false);
+                            
+
+                            if(hasVirus == true) 
+                            {
+                                alert('This video file is contain Viruss');
+                                inputRef.value = ''; // delete value of input file
+                            }
+                            else 
+                            {
+                                alert('This video file is not contain Viruss.');
+                            }
+                        } catch(error) {
+                            setLoading(false);
+                            alert('Error! An error occurred. Please try again later.')
+                        }
+                    }
+                    await waitingToLoading(inputRef.current);
+                    
                 }
                 else
                 {
@@ -82,7 +171,7 @@ export default function index() {
         console.log('Dropped video file: ', videoFile.name);
     };
 
-    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const handleInputChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
         const files = event.target.files;
         if(files != null)
         {
@@ -90,6 +179,28 @@ export default function index() {
             if(handleFileSelect(files[0]) === true)
             {
                 alert('This is video file.');
+                const waitingToLoading = async (event : React.ChangeEvent<HTMLInputElement>) => {
+                    try {
+                        setLoading(true);
+                        const hasVirus = await checkFileForViruses(files[0]);
+                        setLoading(false);
+                        
+
+                        if(hasVirus == true) 
+                        {
+                            alert('This video file is contain Viruss');
+                            event.target.value = ''; // delete value of input file
+                        }
+                        else 
+                        {
+                            alert('This video file is not contain Viruss.');
+                        }
+                    } catch(error) {
+                        setLoading(false);
+                        alert('Error! An error occurred. Please try again later.')
+                    }
+                }
+                await waitingToLoading(event);
             }
             else
             {
@@ -107,17 +218,19 @@ export default function index() {
     }
     
 
-    // handle files when the input field is changed 
-    // const clamav = new ClamAV();
-
-
-
-
-
 
 
     return (
         <>
+            {
+            loading && 
+            <div className="w-[100%] h-[100%] bg-[#0b0b0bc2] absolute top-0 left-0 ">
+                <div role="status" className="w-[100%] h-[100%] flex flex-col justify-center items-center">
+                    <svg aria-hidden="true" className="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/><path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/></svg>
+                    <span className="font-bold text-[20px] text-[#ffffff] ">The system are checking Viruss in your file. Please waiting the minutes.</span>
+                </div>
+            </div>
+            }
             <div className="w-[100%] h-[100%]  flex flex-col justify-center items-center ">
                 <div className="w-[80%] h-[100%]  flex flex-col ">
                     <div className="w-[100%] h-[100px]  flex justify-start items-center  ">
