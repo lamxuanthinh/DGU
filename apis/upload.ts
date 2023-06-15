ff34619af142b0fee442b99aa370bf7408e9039b
your name <a9tttungdz@gmail.com>
2023-06-15 16:18:34 +0700

    return (#39)

--- apis/upload.ts
+++ apis/upload.ts
@@ -11,16 +11,10 @@ export const upload = {
             return config;
         });
         
-        // return await axiosClient.post("/api/v3/files", formData);
+        return await axiosClient.post("/api/v3/files", formData); 
 
 
-        // test
-        const headers = {
-            "x-apikey": '50d5e74f6630f739b2c71da3d34baab3b4e9a1dbf748a9721d60014f3ef2b4ef',
-            "Content-Type": "multipart/form-data",
-        };
         
-        return await axios.post("https://www.virustotal.com/api/v3/files", formData, {headers});
     },
 
     getMd5: async (id: string) => {
