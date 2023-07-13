bdc40e1bd2e8e3f11483c70d84859963951e4867
Giang-st1-00 <bin17022003@gmail.com>
2023-07-14 01:33:34 +0700

    refactor: file test.tsx

--- pages/test/index.tsx
+++ pages/test/index.tsx
@@ -1,15 +1,10 @@
 import React from "react";
 import dynamic from "next/dynamic";
-import Comments from "@/components/common/Comments/Comments";
 
 const Profile = dynamic((): any => import("@/Views/Test/profile"), {
     ssr: false,
 });
 
 export default function ProfilePage() {
-    return (
-        <div className="h-screen w-screen bg-[#333] ">
-            <Comments currentUserId="1" />;
-        </div>
-    );
+    return <div className="h-screen w-screen bg-[#333] ">{/* <Comments currentUserId="1" />; */}</div>;
 }
