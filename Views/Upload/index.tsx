c307c7da6886d2d7e69acbcffd842c9c2b0a73c7
DuyTinh2003 <tranduytinh258@gmail.com>
2023-09-24 20:53:51 +0700

    fix: layout upload (#188)

--- Views/Upload/index.tsx
+++ Views/Upload/index.tsx
@@ -4,6 +4,7 @@ import Modal from "../../components/common/Modal";
 import { FaCloudUploadAlt } from "react-icons/fa";
 import { useAppContext } from "@/Context";
 import ModalSelectCourse from "@/components/common/ModalSelectCourse";
+import Button from "@/components/common/Button";
 
 export default function Upload() {
     const {
@@ -19,7 +20,6 @@ export default function Upload() {
     const { push } = useRouter();
     const [isModal, setIsModal] = useState<boolean>(false);
     const [isCloseModal, setIsCloseModal] = useState<boolean>(false);
-    const [isDragging, setIsDragging] = useState(false);
 
     const canvasRef = useRef<HTMLCanvasElement>(null);
     const videoRef = useRef<HTMLVideoElement>(null);
@@ -70,33 +70,29 @@ export default function Upload() {
         setIsCloseModal(false);
     };
 
-    const handleDragOver = (event: React.DragEvent<HTMLDivElement>) => {
-        event.preventDefault();
-        setIsDragging(true);
-    };
-
-    const handleDragLeave = () => {
-        setIsDragging(false);
-    };
-
     const handleDrop = async (event: React.DragEvent<HTMLDivElement>) => {
         event.preventDefault();
-        setIsDragging(false);
         const files = event.dataTransfer.files;
-        if (inputRef.current) {
-            inputRef.current.files = event.dataTransfer.files;
-            if (inputRef.current.files.length > 0) {
-                if (handleFileSelect(inputRef.current.files[0]) === true) {
-                    setIsModal(true);
-                    const blob = new Blob([files[0]], { type: "image/png" });
-                    const url = URL.createObjectURL(blob);
-                    setSrcVideoEdit(url);
-                } else {
-                    alert("This is not video file.");
-                    inputRef.current.value = "";
+        if (files != null) {
+            if (handleFileSelect(files[0]) === true) {
+                const blob = new Blob([files[0]], { type: "image/png" });
+                const url = URL.createObjectURL(blob);
+                if (videoRef.current) {
+                    videoRef.current.src = URL.createObjectURL(blob);
+                    videoRef.current.load();
+                    videoRef.current.onloadeddata = () => {
+                        if (canvasRef.current && videoRef.current) {
+                            canvasRef.current.width = videoRef.current.videoWidth;
+                            canvasRef.current.height = videoRef.current.videoHeight;
+                        }
+                    };
                 }
+                setSrcVideoEdit(url);
+                setFileVideoUpload(files[0]);
+                setStepSelected(0);
+                setRenderSelectCourse(!isRenderSelectCourse);
             } else {
-                console.log("Không có file được chọn");
+                alert("This is not video file.");
             }
         }
     };
@@ -151,53 +147,30 @@ export default function Upload() {
 
     return (
         <>
-            <div className="w-[100%] h-[100%] flex flex-col justify-center items-center">
-                <div className="sm:w-[80%] h-[100%] flex flex-col w-full">
-                    <div className="h-[100px]  flex justify-start items-center mt-[20px] w-full">
-                        <p className="font-bold text-[25px] w-full mx-auto">Upload your file below</p>
+            <div className="px-36 py-14 w-full h-full flex flex-col">
+                <h1 className="text-[25px] font-bold mb-11">Upload your file bellow</h1>
+                <div
+                    onDrop={handleDrop}
+                    className="relative flex flex-col items-center gap-y-5 w-full border-2 border-black border-dashed h-full pt-[70px] pb-20"
+                >
+                    <div className="w-[140px]">
+                        <FaCloudUploadAlt className="w-full h-full flex items-center justify-center opacity-60" />
                     </div>
-                    <div className="w-[100%] h-[70%] flex justify-center items-center">
-                        <div
-                            className={`w-full h-[95%] border-dashed border-2 border-black flex justify-center items-center drag-drop-area 
-                            ${isDragging ? "dragging" : ""}
-                            `}
-                            onDragOver={handleDragOver}
-                            onDragLeave={handleDragLeave}
-                            onDrop={handleDrop}
-                        >
-                            <div className="w-[50%] h-[70%]  flex flex-col justify-center items-center ">
-                                <div className="w-[100%] h-1/3 flex flex-col justify-center items-center">
-                                    <FaCloudUploadAlt
-                                        className="h-full w-full flex items-center justify-center opacity-60"
-                                        size={100}
-                                    />
-                                </div>
-                                <div className="w-[100%] h-1/3 mt-[6px] mb-[10px] flex flex-col justify-center items-center">
-                                    <div className="w-[100%] h-[60px] my-[10px]  flex justify-center items-end">
-                                        <p className="font-bold  text-2xl text-center">Drag your file here</p>
-                                    </div>
-                                    <div className="w-[100%] h-[60px] my-[10px]  flex justify-center items-start">
-                                        <p className="font-bold  text-2xl ">Or</p>
-                                    </div>
-                                </div>
-                                <div className="w-[100%] h-1/3 my-[10px]  flex justify-center items-center">
-                                    <input
-                                        className="hidden"
-                                        type="file"
-                                        accept="video/*"
-                                        ref={inputRef}
-                                        onChange={handleInputChange}
-                                    />
-                                    <div
-                                        onClick={handleButtonClick}
-                                        className="w-[180px] h-[70px] border-2 border-solid border-[#000000] flex justify-center items-center cursor-pointer"
-                                    >
-                                        <p className="font-bold text-[25px]">Browser</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    <h2 className="text-2xl font-bold">Drag your file here</h2>
+                    <h2 className="text-2xl font-bold">Or</h2>
+                    <input
+                        className="hidden"
+                        type="file"
+                        accept="video/*"
+                        ref={inputRef}
+                        onChange={handleInputChange}
+                    />
+                    <Button
+                        onClick={handleButtonClick}
+                        className="border-2 border-solid border-black w-[180px] h-[70px] text-[25px] font-bold"
+                    >
+                        Browser
+                    </Button>
                 </div>
             </div>
             {isModal && <Modal title="Do you want to go to the video editing step?" onOk={onOk} onCancel={onCancel} />}
