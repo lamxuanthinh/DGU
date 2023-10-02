57ddec220bd38d03272cebbdfbd4aa2f67e908cc
DuyTinh2003 <tranduytinh258@gmail.com>
2023-10-02 21:38:53 +0700

    fix: verify email

--- Views/Page404/index.tsx
+++ Views/Page404/index.tsx
@@ -5,7 +5,7 @@ import image404 from "@/public/Images/404/404.png";
 
 const Page404 = () => {
     return (
-        <div className="lg:px-24 lg:py-24 md:py-20 md:px-44 px-4 py-24 items-center flex justify-center flex-col-reverse lg:flex-row md:gap-28 gap-16">
+        <div className="lg:px-24 lg:py-24 md:py-20 md:px-44 px-4 py-24 items-center flex justify-center flex-col-reverse lg:flex-row md:gap-28 gap-16 animate-falseIn">
             <div className="xl:pt-24 w-full xl:w-1/2 relative pb-12 lg:pb-0">
                 <div className="relative">
                     <div className="absolute z-10">
