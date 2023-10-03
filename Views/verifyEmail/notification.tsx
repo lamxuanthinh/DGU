ed32cca3939a3317e9af1d74b8b44c96fc241d87
DuyTinh2003 <tranduytinh258@gmail.com>
2023-10-03 20:34:48 +0700

    fix: clear time out and edit loading

--- Views/verifyEmail/notification.tsx
+++ Views/verifyEmail/notification.tsx
@@ -2,10 +2,24 @@ import { IQueryNotification } from "@/model";
 import { useRouter } from "next/router";
 import VerifyAuth from "@/components/common/VerifyAuth";
 import notificationImage from "@/public/Images/Verify/notification.png";
+import { useEffect, useState } from "react";
+import Loading from "@/components/common/Loading";
+import Page404 from "../Page404";
 
 export default function Notification() {
     const router = useRouter();
     const { emailSent } = router.query as IQueryNotification;
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+
+    useEffect(() => {
+        const timerWait = setTimeout(() => {
+            setIsLoading(false);
+        }, 500);
+        return () => {
+            clearTimeout(timerWait);
+        };
+    }, []);
+
     const description = `We've sent a verification email to ${
         emailSent && encodeEmail(emailSent)
     }. You need to verify your email address to log into our Vebsite.`;
@@ -24,5 +38,23 @@ export default function Notification() {
         return encodedEmail;
     }
 
-    return <VerifyAuth heading="Verify your email" description={description} srcImage={notificationImage} />;
+    return (
+        <>
+            {isLoading ? (
+                <Loading isIcon={false} />
+            ) : (
+                <>
+                    {emailSent ? (
+                        <VerifyAuth
+                            heading="Verify your email"
+                            description={description}
+                            srcImage={notificationImage}
+                        />
+                    ) : (
+                        <Page404 />
+                    )}
+                </>
+            )}
+        </>
+    );
 }
