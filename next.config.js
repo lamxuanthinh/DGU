9f6d61397bda825616ec8003aa88b01d4d5647a1
Jocelyn Lam <94306642+lamxuanthinh@users.noreply.github.com>
2023-10-13 22:41:11 +0700

    fix: adjust next config

--- next.config.js
+++ next.config.js
@@ -1,30 +1,10 @@
 /** @type {import("next").NextConfig} */
 
-const getCorsHeaders = () => {
-    const headers = {};
-
-    headers["Access-Control-Allow-Origin"] = "*";
-    headers["Access-Control-Allow-Credentials"] = "true";
-    headers["Access-Control-Allow-Methods"] = "GET,OPTIONS,PATCH,DELETE,POST,PUT";
-    headers["Access-Control-Allow-Headers"] =
-        "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization";
-
-    return Object.entries(headers).map(([key, value]) => ({ key, value }));
-};
-
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
     images: {
         domains: ["papik.pro", "player.vdocipher.com", "res.cloudinary.com"],
-    },
-    async headers() {
-        return [
-            {
-                source: "/api/(.*)",
-                headers: getCorsHeaders(),
-            },
-        ];
     },
 };
 
