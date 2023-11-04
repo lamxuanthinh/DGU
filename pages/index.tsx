76edbb1ea2367061678befd6e00f48e84e8794b7
DuyTinh2003 <tranduytinh258@gmail.com>
2023-11-04 19:06:42 +0700

    test: commnet code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,27 @@
-import videoShortApi from "@/apis/videoshort";
+// import videoShortApi from "@/apis/videoshort";
 import MainLayout from "@/components/layout/MainLayout";
-import Home from "@/Views/Home";
+// import Home from "@/Views/Home";
 import { GetStaticProps } from "next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-import Head from "next/head";
-import { useEffect } from "react";
+// import Head from "next/head";
+// import { useEffect } from "react";
 
 const HomePage = ({ posts }: any) => {
-    useEffect(() => {
-        const setFullHeight = () => {
-            const vh = window.innerHeight * 0.01;
-            document.documentElement.style.setProperty("--vh", `${vh}px`);
-        };
+    // useEffect(() => {
+    //     const setFullHeight = () => {
+    //         const vh = window.innerHeight * 0.01;
+    //         document.documentElement.style.setProperty("--vh", `${vh}px`);
+    //     };
 
-        setFullHeight();
-        window.addEventListener("resize", setFullHeight);
+    //     setFullHeight();
+    //     window.addEventListener("resize", setFullHeight);
 
-        return () => window.removeEventListener("resize", setFullHeight);
-    }, []);
+    //     return () => window.removeEventListener("resize", setFullHeight);
+    // }, []);
 
     return (
         <>
-            <Head>
+            {/* <Head>
                 <title>DGU</title>
                 <link rel="icon" href="/icon?<generated>" type="image/<generated>" sizes="<generated>" />
                 <link
@@ -30,7 +30,8 @@ const HomePage = ({ posts }: any) => {
                 />
                 <meta name="viewport" content="initial-scale=1.0, width=device-width" />
             </Head>
-            <Home data={posts} />
+            <Home data={posts} /> */}
+            <h1>home page</h1>
         </>
     );
 };
@@ -40,55 +41,55 @@ HomePage.Layout = MainLayout;
 export default HomePage;
 
 export const getStaticProps: GetStaticProps = async ({ locale }) => {
-    let posts: any;
+    // let posts: any;
 
-    const extractVideoData = (data: any[]) => {
-        return data.map((item) => {
-            const numbers = item.split(":").map(Number);
-            const point = numbers[0];
-            const duration = numbers[1] - numbers[0];
+    // const extractVideoData = (data: any[]) => {
+    //     return data.map((item) => {
+    //         const numbers = item.split(":").map(Number);
+    //         const point = numbers[0];
+    //         const duration = numbers[1] - numbers[0];
 
-            return {
-                point,
-                duration,
-            };
-        });
-    };
+    //         return {
+    //             point,
+    //             duration,
+    //         };
+    //     });
+    // };
 
-    const extractStringVideoData = (item: string) => {
-        const numbers = item.split(":").map(Number);
-        const point = numbers[0];
-        const duration = numbers[1] - numbers[0];
+    // const extractStringVideoData = (item: string) => {
+    //     const numbers = item.split(":").map(Number);
+    //     const point = numbers[0];
+    //     const duration = numbers[1] - numbers[0];
 
-        return {
-            point,
-            duration,
-        };
-    };
+    //     return {
+    //         point,
+    //         duration,
+    //     };
+    // };
 
-    try {
-        const { metaData } = await videoShortApi.getAllVideoShort();
+    // try {
+    //     const { metaData } = await videoShortApi.getAllVideoShort();
 
-        posts = metaData.shortList.slice(0, 2);
+    //     posts = metaData.shortList.slice(0, 2);
 
-        for (let i = 0; i < posts.length; i++) {
-            const parentId = posts[i].videoPublicId;
-            posts[i].controlData = extractStringVideoData(posts[i].shortTimeLine);
+    //     for (let i = 0; i < posts.length; i++) {
+    //         const parentId = posts[i].videoPublicId;
+    //         posts[i].controlData = extractStringVideoData(posts[i].shortTimeLine);
 
-            const { metaData } = await videoShortApi.getVideoById(parentId);
+    //         const { metaData } = await videoShortApi.getVideoById(parentId);
 
-            posts[i].fullVideoInfo = metaData.publicVideo;
+    //         posts[i].fullVideoInfo = metaData.publicVideo;
 
-            posts[i].fullVideoInfo.controlData = extractVideoData(posts[i].fullVideoInfo.shortTimeLine);
-        }
-    } catch (error) {
-        console.log("Error during video home:", error);
-        posts = [];
-    }
+    //         posts[i].fullVideoInfo.controlData = extractVideoData(posts[i].fullVideoInfo.shortTimeLine);
+    //     }
+    // } catch (error) {
+    //     console.log("Error during video home:", error);
+    //     posts = [];
+    // }
 
     return {
         props: {
-            posts,
+            // posts,
             ...(await serverSideTranslations(locale ?? "vi", ["layout"])),
         },
         revalidate: 60,
