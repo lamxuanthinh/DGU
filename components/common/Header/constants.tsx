97d6e226386a3305d24d84396b6cda720c775e9c
Giang-st1-00 <bin17022003@gmail.com>
2023-12-22 00:26:24 +0700

    feat: add notification

---
+++ components/common/Header/constants.tsx
@@ -0,0 +1,30 @@
+import { INotificationItems } from "@/model/notification";
+
+export const dataNotification: Array<INotificationItems> = [
+    {
+        avatar: "boy_thoi_trang.png",
+        name: "Jane Smith",
+        type: 1,
+        lastMessageLine: "Bài viết rất hay! Cảm ơn bạn đã chia sẻ",
+        time: "1 minute ago",
+    },
+    {
+        avatar: "boy_thanh_lich.png",
+        name: "Jocelyn Lam",
+        type: 2,
+        time: "12 minutes ago",
+    },
+    {
+        avatar: "boy_thanh_lich.png",
+        name: "Eira",
+        type: 3,
+        time: "31 minutes ago",
+    },
+    {
+        avatar: "boy_thoi_trang.png",
+        name: "Yang",
+        type: 4,
+        lastMessageLine: "Làm giàu",
+        time: "5 hours ago",
+    },
+];
